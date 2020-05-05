874cae2b67a63dc1381d250866478d6530e6cddf
Pranav Kumar <pbpranav24@gmail.com>
2020-05-05 17:03:30 +0530

    Updates

---
+++ precache-manifest.625840d49bbf605a35fb7c63dd8fec0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6904fef134ee5537ad574e7e491a8fe4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c77afed39720a0509e5",
+    "url": "/static/css/main.fe8ef87c.chunk.css"
+  },
+  {
+    "revision": "084397231dc1be88c15b",
+    "url": "/static/js/2.fe8a8690.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.fe8a8690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c77afed39720a0509e5",
+    "url": "/static/js/main.1bbfb3cf.chunk.js"
+  },
+  {
+    "revision": "d43de932321dcfe65b00",
+    "url": "/static/js/runtime-main.92bd8cb9.js"
+  }
+]);
