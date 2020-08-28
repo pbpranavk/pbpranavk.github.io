a733915214862613c8a07ec7906c8e88e05c38ac
pbpranavk <pbpranav24@gmail.com>
2020-08-28 19:34:44 +0530

    Updates

--- precache-manifest.cd9be682640cf561e9fbf39b12a1e748.js
+++ precache-manifest.cd9be682640cf561e9fbf39b12a1e748.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3f2c8a567dd1d25efcd00c9a3fe87a2f",
+    "revision": "1d48b55da679d5c0ceea187a45c76bed",
     "url": "/index.html"
   },
   {
-    "revision": "e4ac237deb3bf0a4e82c",
+    "revision": "35497b80610fe03fa28a",
     "url": "/static/css/main.37105a9c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.77778e8c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e4ac237deb3bf0a4e82c",
-    "url": "/static/js/main.446498e6.chunk.js"
+    "revision": "35497b80610fe03fa28a",
+    "url": "/static/js/main.32813a10.chunk.js"
   },
   {
     "revision": "eb81441c62b43b4493e5",
