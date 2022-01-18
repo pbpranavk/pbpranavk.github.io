874d89094ed40a4e6343ee1b32f8f29f3b9d0ddf
Pranav Kumar <pbpranav24@gmail.com>
2022-01-18 22:49:04 +0530

    Updates

--- precache-manifest.4f8c1af342443606ce2997b0de0210ea.js
+++ precache-manifest.4f8c1af342443606ce2997b0de0210ea.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "08f350f41d96ae95c969b354af8f52a7",
+    "revision": "906219edc4eb496ac0199841f65b5777",
     "url": "/index.html"
   },
   {
-    "revision": "e211c3fbae6683729fff",
+    "revision": "22ac72e9f0663d06fc29",
     "url": "/static/css/main.d0c3f4e1.chunk.css"
   },
   {
-    "revision": "573d9e2a420194127e10",
-    "url": "/static/js/2.06d64c1c.chunk.js"
+    "revision": "456cbdb241414436145f",
+    "url": "/static/js/2.be013994.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/static/js/2.06d64c1c.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.be013994.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e211c3fbae6683729fff",
-    "url": "/static/js/main.1374823d.chunk.js"
+    "revision": "22ac72e9f0663d06fc29",
+    "url": "/static/js/main.975f61ba.chunk.js"
   },
   {
     "revision": "eb81441c62b43b4493e5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.db978265ef15b1e29a8e79910231059f.js"
+  "/precache-manifest.4f8c1af342443606ce2997b0de0210ea.js"
 );
 
 self.addEventListener('message', (event) => {
